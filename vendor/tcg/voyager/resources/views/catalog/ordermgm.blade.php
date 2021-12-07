4d2d6350cfd940b5f3fb681169e4e99567b53489
nsakib9 <sakibkhan207@gmail.com>
2021-12-07 09:37:57 +0600

    order table bug fix

--- vendor/tcg/voyager/resources/views/catalog/ordermgm.blade.php
+++ vendor/tcg/voyager/resources/views/catalog/ordermgm.blade.php
@@ -21,7 +21,7 @@
             <div class="col-md-12">
                 <div class="panel panel-bordered">
                   @if(Session::has('message')) <div class="alert alert-info"> {{Session::get('message')}} </div> @endif
-                    
+
                     <div class="panel-body">
                        <div class="col-md-12" id="downloadexceldiv" align="center">
                           <label for="downloadexcel">Export to:</label>
@@ -56,19 +56,19 @@
                                 </tr>
                             </thead>
                              <tbody>
-                             @foreach($data as $d) 
+                             @foreach($data as $d)
                             <tr>
                                 <td class="order_ID">{{$d->id}}</td>
                                    <?php $user = App\User::userinfo($d->user_id); ?>
-                                <td>{{$user->name}}</td>
+                                <td>{{@$user->name}}</td>
                                  <td>${{$d->od_amount}}</td>
                                  <td>${{$d->shipping_amt}}</td>
                                 <td>${{$d->coupon_amt}}</td>
                                 <td>{{ substr($d->created_at, 0, 10)}}</td>
                                 <td style="text-align:center" class="no-sort no-click">
                                   <?php $check = App\Http\Helpers::allItemsCancel($d->id); ?>
-                                 
-                                  @if($check == 1 || $check == 2)  
+
+                                  @if($check == 1 || $check == 2)
                                  @if($d->request_status == 1)
                                   <div class="btn-sm btn-danger pull-right" data-toggle="modal" data-id="{{$d->id}}" id="delete-{{$d->id}}" data-target="#requestCancel{{$d->id}}">
                                     <i class="voyager-check-circle"></i> Pending Cancellation
@@ -93,15 +93,15 @@
                                      @else
                                      @endif
                                  @endif
-                                  
+
                                    @else
-                                     
+
                                     @endif
 
                                  </td>
                                  <td style="text-align:center">
-                                  @if($d->orderstatus == 1)  
-                                 
+                                  @if($d->orderstatus == 1)
+
                                   <div class="btn-sm btn-danger pull-right" data-toggle="modal" data-id="{{$d->id}}" id="delete-{{$d->id}}" data-target="#ProcessRequest{{$d->id}}">
                                     <i class="voyager-check-circle"></i> Order placed
                                 </div>
@@ -135,18 +135,18 @@
                                 </td>
                                          <td class="no-sort no-click">
                                            <a href="{{ url('/storage/pdf')}}/{{$d->id}}-invoice.pdf" target="_blank" class="btn-sm btn-primary pull-right edit">
-                                                <i class="voyager-receipt"></i> 
+                                                <i class="voyager-receipt"></i>
                                             </a>
-                                            
-                                            
+
+
                                         </td>
                                         <td><div class="btn-sm btn-danger pull-right" data-toggle="modal" data-id="{{$d->id}}" id="delete-{{$d->id}}" data-target="#SmsRequest{{$d->id}}">
                                    <i class="voyager-phone"></i>
                                 </div> </td>
                                  <td><a href="{{ route('voyager.orderitems')}}/{{$d->id}}" class="btn-sm btn-info pull-right edit"><i class="voyager-eye"></i> </a></td>
-                                 
+
                             </tr>
-                           
+
                             <div class="modal fade requestCancel" id="requestCancel{{$d->id}}" role="dialog">
                              <div class="modal-dialog">
                                <!-- Modal content-->
@@ -173,14 +173,14 @@
                                   <?php  $rq = App\Http\Helpers::get_user_request_status($d->id); ?>
                                    <div class="modal-body">
                                    <form class="" role="form" method="POST" action="{{ route('voyager.orderrecords.cancelrequest') }}">
-                                      
+
                                        {{ csrf_field() }}
                                      <div class="form-group row">
                                        <label for="example-text-input" class="col-sm-3 col-form-label">Reason:</label>
                                        <div class="col-sm-9">
-                                         <input type="hidden" value="{{$d->id}}" name="order_id">             
+                                         <input type="hidden" value="{{$d->id}}" name="order_id">
                                         <select name="request_status" class="form-control select" required>
-                                           <option value="">Select Status</option>                                           
+                                           <option value="">Select Status</option>
                                             <option value="2" @if(isset($rq) && $rq == "2") selected @endif>Approved</option>
                                             <option value="3" @if(isset($rq) && $rq == "3") selected @endif>Disapproved</option>
                                             <option value="4" @if(isset($rq) && $rq == "4") selected @endif>Refund</option>
@@ -191,9 +191,9 @@
                                           <textarea class="form-control" name="decs_reason" required></textarea>
                                       </div>
                                 </div>
-                                
+
                             <button type="submit" class="btn btn-info pull-left">Submit</button>
-                           
+
                         </form>
                         &nbsp; <button type="button" class="btn btn-info pull-left" data-dismiss="modal" style="margin-left: 10px;">Cancel</button>
                     </div>
@@ -201,7 +201,7 @@
 
             </div>
         </div>
-        
+
         <!-- Process Request -->
          <div class="modal fade ProcessRequest" id="ProcessRequest{{$d->id}}" role="dialog">
                              <div class="modal-dialog">
@@ -214,15 +214,15 @@
                                   <?php $os = App\Http\Helpers::get_user_order_status($d->id); ?>
                                    <div class="modal-body">
                                    <form class="" role="form" method="POST" action="{{ route('voyager.orderrecords.changestatus') }}">
-                                      
+
                                        {{ csrf_field() }}
                                      <div class="form-group row">
                                        <label for="example-text-input" class="col-sm-3 col-form-label">Order Status:</label>
                                        <div class="col-sm-9">
-                                         <input type="hidden" value="{{$d->id}}" name="order_id">             
+                                         <input type="hidden" value="{{$d->id}}" name="order_id">
                                         <select name="orderstatus" class="form-control select" required>
                                              <option value="">Select Status</option>
-                                          
+
                                             <option value="6" @if(isset($os) && $os == "6")  selected  @endif>Approved</option>
                                             <option value="7" @if(isset($os) && $os == "7")  selected  @endif>Disapproved</option>
                                             <option value="2" @if(isset($os) && $os == "2")  selected  @endif>Processing</option>
@@ -236,10 +236,10 @@
                                           <textarea class="form-control" name="user_message" required></textarea>
                                       </div>
                                 </div>
-                                
+
                             <button type="submit" class="btn btn-info pull-left" name="save" value="save">Save</button> &nbsp;
                             <button type="submit" class="btn btn-success pull-left" name="notify" value="notify" style="margin-left:10px;">Notify</button>
-                           
+
                         </form>
                         &nbsp; <button type="button" class="btn btn-cancel pull-left" data-dismiss="modal" style="margin-left: 10px;">Cancel</button>
                     </div>
@@ -250,7 +250,7 @@
         <!-- End process Request -->
           <!-- Sms Request -->
          <div class="modal fade SmsRequest" id="SmsRequest{{$d->id}}" role="dialog">
-                             <div class="modal-dialog">	
+                             <div class="modal-dialog">
                                <!-- Modal content-->
                                <div class="modal-content" style="padding-bottom: 20px;">
                                  <div class="modal-header">
@@ -277,21 +277,21 @@
                                    <?php } ?>
                                    <div class="modal-body">
                                    <form class="" role="form" method="POST" action="{{ route('voyager.orderrecords.ordersms') }}">
-                                      
+
                                        {{ csrf_field() }}
                                      <div class="form-group row">
-                                       
-                                         <input type="hidden" value="{{$d->id}}" name="order_id">             
-                                        
-                                     
+
+                                         <input type="hidden" value="{{$d->id}}" name="order_id">
+
+
                                       <label for="example-text-input" class="col-sm-3 col-form-label">Sms Message:</label>
                                        <div class="col-sm-9">
                                           <textarea class="form-control" name="sms_message" required></textarea>
                                       </div>
                                 </div>
-                                
+
                             <button type="submit" class="btn btn-info pull-left">Send Sms</button>
-                           
+
                         </form>
                         &nbsp; <button type="button" class="btn btn-info pull-left" data-dismiss="modal" style="margin-left: 10px;">Cancel</button>
                     </div>
@@ -304,7 +304,7 @@
              @endforeach
                                 </tbody>
                             </table>
-                            
+
                     </div>
                 </div>
             </div>
@@ -317,7 +317,7 @@
        $(document).ready(function () {
                 $('#dataTable').DataTable({ "order": [] });
         });
-    
+
       $('select#downloadexcel').on('change', function() {
 
         if($(this).val() !=''){
@@ -330,7 +330,7 @@
                   $a[0].click();
                   $a.remove();
 
-         
+
         }
 
       });
@@ -338,7 +338,7 @@
       $('#search_by_range').on('click',function(){
         var date1 = $("#from").val();
         var date2 = $("#to").val();
-        var flag = 0; 
+        var flag = 0;
 
         $('#daterange_div input').each(function(){
           flag = 0;
@@ -356,9 +356,9 @@
           $('#from').blur();
           $('#to').blur();
         }
-        
+
       });
-     
+
 
     $( function() {
 
@@ -382,7 +382,7 @@
       .on( "change", function() {
         from.datepicker( "option", "maxDate", getDate( this ) );
       });
- 
+
     function getDate( element ) {
         var date;
         try {
@@ -390,7 +390,7 @@
         } catch( error ) {
           date = null;
         }
-   
+
         return date;
       }
 
@@ -402,13 +402,13 @@
         var iFfin = document.getElementById('to').value;
         var iStartDateCol = 5;
         var iEndDateCol = 5;
- 
+
         iFini=iFini.substring(6,10) + iFini.substring(3,5)+ iFini.substring(0,2);
         iFfin=iFfin.substring(6,10) + iFfin.substring(3,5)+ iFfin.substring(0,2);
- 
+
         var datofini=aData[iStartDateCol].substring(6,10) + aData[iStartDateCol].substring(3,5)+ aData[iStartDateCol].substring(0,2);
         var datoffin=aData[iEndDateCol].substring(6,10) + aData[iEndDateCol].substring(3,5)+ aData[iEndDateCol].substring(0,2);
- 
+
         if ( iFini === "" && iFfin === "" )
         {
             return true;
@@ -428,7 +428,7 @@
         return false;
     }
 );
-  
+
   $(document).ready(function() {
       var table = $('#dataTable').DataTable();
       // Add event listeners to the two range filtering inputs
